849a0da3c1a240427e6f10d077a3d4ce270c5701
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-30 10:59:59 +0300

    модель записи в базе данных

--- app/src/main/java/com/example/carcollectionapp/data/dataBase/CarInfoDbModel.kt
+++ app/src/main/java/com/example/carcollectionapp/data/dataBase/CarInfoDbModel.kt
@@ -1,3 +1,14 @@
 package com.example.carcollectionapp.data.dataBase
 
-data class CarInfoDbModel(val a: Int)
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "cars")
+data class CarInfoDbModel(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val picturePath: String,
+    val productionDate: String,
+    val engineCapacity: Int,
+    val insertionDate: String
+)
