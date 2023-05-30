55a33b6a131a73e66bddcaaf029b18ea19f6e056
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-30 14:33:51 +0300

    Класс для настройки бд

--- app/src/main/java/com/example/carcollectionapp/data/dataBase/AppDataBase.kt
+++ app/src/main/java/com/example/carcollectionapp/data/dataBase/AppDataBase.kt
@@ -1,4 +1,41 @@
 package com.example.carcollectionapp.data.dataBase
 
-class AppDataBase {
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [CarInfoDbModel::class],
+    version = 0, exportSchema = false
+)
+
+abstract class AppDatabase : RoomDatabase() {
+    companion object {
+
+        private var INSTANCE: AppDatabase? = null
+        private const val DB_NAME = "CarCollection.db"
+        private val LOCK = Any()
+
+        fun getInstance(application: Application): AppDatabase {
+            INSTANCE?.let {
+                return it
+            }
+            synchronized(LOCK) {
+                INSTANCE?.let { return it }
+                val instance =
+                    Room.databaseBuilder(
+                        application,
+                        AppDatabase::class.java,
+                        DB_NAME
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
+    abstract fun carInfoDao(): CarInfoDao
 }
