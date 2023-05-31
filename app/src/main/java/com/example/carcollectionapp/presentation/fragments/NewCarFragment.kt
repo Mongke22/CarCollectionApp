09b08ca7c1b7cb508996d6618dbc9cbb2acb93be
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 16:55:04 +0300

    Реализация экрана

--- app/src/main/java/com/example/carcollectionapp/presentation/fragments/NewCarFragment.kt
+++ app/src/main/java/com/example/carcollectionapp/presentation/fragments/NewCarFragment.kt
@@ -1,24 +1,214 @@
 package com.example.carcollectionapp.presentation.fragments
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.Manifest
+import android.app.Activity
+import android.app.DatePickerDialog
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.navigation.fragment.findNavController
 import com.example.carcollectionapp.R
+import com.example.carcollectionapp.databinding.FragmentNewCarBinding
+import com.example.carcollectionapp.domain.CarInfo
+import com.example.carcollectionapp.presentation.viewmodel.NewCarViewModel
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-class NewCarFragment : Fragment() {
+class NewCarFragment : BaseFragment<FragmentNewCarBinding, NewCarViewModel>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_new_car, container, false)
-    }
     companion object {
+        private const val GALLERY = 1
+        private const val CAMERA = 2
+        private const val IMAGE_DIRECTORY = "cars"
 
         fun newInstance() = NewCarFragment()
     }
+
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    private var theNewCar = CarInfo(
+        id = 0,
+        carName = "",
+        picturePath = "",
+        productionDate = "",
+        engineCapacity = 0,
+        insertionDate = ""
+    )
+
+    override fun getViewBinding(): FragmentNewCarBinding {
+        return FragmentNewCarBinding.inflate(layoutInflater)
+    }
+
+    override fun getViewModelClass(): Class<NewCarViewModel> {
+        return NewCarViewModel::class.java
+    }
+
+    override fun setupView() {
+        setupDatePickerListener()
+        setupClickListeners()
+    }
+
+    private fun setupDatePickerListener() {
+        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            val myFormat = resources.getString(R.string.date_format)
+            val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+            binding.etProductionDate.setText(sdf.format(calendar.time).toString())
+        }
+    }
+
+    private fun setupClickListeners() {
+
+        setupAddImageClickListener()
+        setupDateChooseClickListener()
+        setupSaveButtonListener()
+
+    }
+
+    private fun setupAddImageClickListener() {
+        binding.tvAddImage.setOnClickListener {
+            val pictureDialog = AlertDialog.Builder(requireActivity())
+            pictureDialog.setTitle(resources.getString(R.string.choose_action))
+            val pictureDialogItems = arrayOf(resources.getString(R.string.choose_photo), resources.getString(R.string.choose_gallery))
+            pictureDialog.setItems(pictureDialogItems) { _, which ->
+                when (which) {
+                    0 -> {
+                        choosePhotoFromGallery()
+                    }
+                    1 -> {
+                        takePhotoFromCamera()
+                    }
+                }
+            }
+            pictureDialog.show()
+        }
+    }
+
+    private fun setupDateChooseClickListener() {
+        binding.etProductionDate.setOnClickListener {
+            DatePickerDialog(requireActivity(), dateSetListener,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
+    }
+
+    private fun setupSaveButtonListener(){
+        binding.btnSave.setOnClickListener {
+            viewModel.saveNewCar(theNewCar, findNavController())
+        }
+    }
+
+    private fun choosePhotoFromGallery() {
+        Dexter.withContext(requireActivity()).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    val galleryIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    requireActivity().startActivityForResult(galleryIntent, GALLERY)
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>,
+                token: PermissionToken,
+            ) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check();
+    }
+
+    private fun takePhotoFromCamera() {
+        Dexter.withActivity(requireActivity()).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    val cameraIntent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    startActivityForResult(cameraIntent, CAMERA)
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>,
+                token: PermissionToken,
+            ) {
+                showRationalDialogForPermissions()
+            }
+        }).onSameThread().check();
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                if (data != null) {
+                    val contentURI = data.data
+                    try {
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,
+                                contentURI)
+                        binding.ivImage.setImageBitmap(selectedImageBitmap)
+                        theNewCar.picturePath = saveImageToInternalStorage(selectedImageBitmap)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(requireActivity(), resources.getString(R.string.something_went_wrong), Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            } else if (requestCode == CAMERA) {
+                val thumbNail: Bitmap = data?.extras?.get("data") as Bitmap
+                binding.ivImage.setImageBitmap(thumbNail)
+                theNewCar.picturePath = saveImageToInternalStorage(thumbNail)
+            }
+        }
+    }
+
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(requireActivity())
+            .setMessage(resources.getString(R.string.require_permissions))
+            .setPositiveButton(resources.getString(R.string.confirm)) { dialog, _ ->
+                dialog.dismiss()
+            }.setNegativeButton(resources.getString(R.string.cancel)) { dialog, _ ->
+            dialog.dismiss()
+        }.show()
+    }
+
+    private fun saveImageToInternalStorage(bitmap: Bitmap): String {
+        val wrapper = ContextWrapper(requireActivity())
+        var file = wrapper.getDir(IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        file = File(file, "${UUID.randomUUID()}.jpg")
+        try {
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return file.absolutePath
+    }
+
 }
