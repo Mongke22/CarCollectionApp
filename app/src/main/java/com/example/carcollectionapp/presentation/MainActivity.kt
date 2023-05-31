08b5dc0db11e6687b3e6e426b57a62f0f3e95671
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 14:05:30 +0300

    Добавление перехода на главный фрагмент при запуске

--- app/src/main/java/com/example/carcollectionapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/carcollectionapp/presentation/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.carcollectionapp.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.carcollectionapp.R
+import com.example.carcollectionapp.presentation.fragments.CarsListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.main_fragment_container, CarsListFragment.newInstance())
+            .commit()
     }
 }
