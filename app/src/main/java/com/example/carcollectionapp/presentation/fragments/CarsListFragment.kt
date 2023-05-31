2831af00882e110d568e0c190166e764760b449e
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 14:06:24 +0300

    Реализация логики основного экрана

--- app/src/main/java/com/example/carcollectionapp/presentation/fragments/CarsListFragment.kt
+++ app/src/main/java/com/example/carcollectionapp/presentation/fragments/CarsListFragment.kt
@@ -1,6 +1,66 @@
 package com.example.carcollectionapp.presentation.fragments
 
-import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import com.example.carcollectionapp.databinding.FragmentCarsListBinding
+import com.example.carcollectionapp.presentation.recycler.CarListAdapter
+import com.example.carcollectionapp.presentation.viewmodel.CarsListViewModel
 
-class CarsListFragment: Fragment() {
+class CarsListFragment: BaseFragment<FragmentCarsListBinding, CarsListViewModel>() {
+
+    private lateinit var carListAdapter: CarListAdapter
+
+    companion object{
+        fun newInstance() = CarsListFragment()
+    }
+
+    override fun getViewBinding(): FragmentCarsListBinding {
+        return FragmentCarsListBinding.inflate(layoutInflater)
+    }
+
+    override fun getViewModelClass(): Class<CarsListViewModel> {
+        return CarsListViewModel::class.java
+    }
+
+    override fun setUpOnBackPressed() {
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    requireActivity().finish()
+                }
+            })
+    }
+
+    override fun observeData() {
+        viewModel.carList.observe(this){ cars ->
+            if(cars.isEmpty()){
+                binding.tvNoItems.visibility = View.VISIBLE
+                binding.rvCars.visibility = View.GONE
+            }else{
+                binding.tvNoItems.visibility = View.GONE
+                binding.rvCars.visibility = View.VISIBLE
+                carListAdapter.submitList(cars)
+            }
+        }
+    }
+
+    override fun setupView() {
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView(){
+        val rvCarsList = binding.rvCars
+        with(rvCarsList) {
+            carListAdapter = CarListAdapter()
+            adapter = carListAdapter
+        }
+        setupItemClickListener()
+    }
+
+    private fun setupItemClickListener(){
+        carListAdapter.itemOnClickListener = { car ->
+            viewModel.showCarDetailInfo(car.id)
+        }
+    }
 }
