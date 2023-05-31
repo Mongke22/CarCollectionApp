ebd325ff8f77452ff302c3edbb5c86a5eecd3e0b
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 12:46:50 +0300

    Реаилзация репозитория

--- app/src/main/java/com/example/carcollectionapp/data/CarRepositoryImpl.kt
+++ app/src/main/java/com/example/carcollectionapp/data/CarRepositoryImpl.kt
@@ -1,4 +1,31 @@
 package com.example.carcollectionapp.data
 
-class CarRepositoryImpl {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.carcollectionapp.data.database.AppDataBase
+import com.example.carcollectionapp.domain.CarInfo
+import com.example.carcollectionapp.domain.CarRepository
+
+class CarRepositoryImpl(private val application: Application): CarRepository {
+
+    private val carInfoDao = AppDataBase.getInstance(application).carInfoDao()
+    private val mapper = Mapper()
+
+
+    override fun getCarInfoList(): LiveData<List<CarInfo>> {
+        return Transformations.map(carInfoDao.getCarsInfoList()) {
+            it.map { carInfoDb ->
+                mapper.mapDbModelToEntity(carInfoDb)
+            }
+        }
+    }
+
+    override suspend fun getCarInfo(carId: Int): CarInfo {
+        return mapper.mapDbModelToEntity(carInfoDao.getCarInfo(carId))
+    }
+
+    override suspend fun addCarInfo(car: CarInfo) {
+        carInfoDao.insertCarInfo(mapper.mapEntityToDbModel(car))
+    }
 }
