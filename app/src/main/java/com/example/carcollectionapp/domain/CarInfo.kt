335fc5b81711728cddd2e1be436d5341c14fd036
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 12:29:08 +0300

    Добавление модели для бизнес логики

---
+++ app/src/main/java/com/example/carcollectionapp/domain/CarInfo.kt
@@ -0,0 +1,9 @@
+package com.example.carcollectionapp.domain
+
+data class CarInfo(
+    val id: Int,
+    val picturePath: String,
+    val productionDate: String,
+    val engineCapacity: Int,
+    val insertionDate: String
+)
