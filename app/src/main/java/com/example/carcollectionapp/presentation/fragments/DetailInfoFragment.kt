74e2096ad9c153a18f5436d423183453a8a69564
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 18:16:08 +0300

    Реализация экрана детальной информации

--- app/src/main/java/com/example/carcollectionapp/presentation/fragments/DetailInfoFragment.kt
+++ app/src/main/java/com/example/carcollectionapp/presentation/fragments/DetailInfoFragment.kt
@@ -1,24 +1,69 @@
 package com.example.carcollectionapp.presentation.fragments
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.carcollectionapp.R
+import com.example.carcollectionapp.databinding.FragmentDetailInfoBinding
+import com.example.carcollectionapp.domain.CarInfo
+import com.example.carcollectionapp.presentation.viewmodel.DetailInfoViewModel
 
 
-class DetailInfoFragment : Fragment() {
+class DetailInfoFragment : BaseFragment<FragmentDetailInfoBinding, DetailInfoViewModel>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_info, container, false)
+    private val args by navArgs<DetailInfoFragmentArgs>()
+
+    override fun getViewBinding(): FragmentDetailInfoBinding {
+        return FragmentDetailInfoBinding.inflate(layoutInflater)
     }
 
-        fun newInstance() = DetailInfoFragment()
+    override fun getViewModelClass(): Class<DetailInfoViewModel> {
+        return DetailInfoViewModel::class.java
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.getCar(args.carId)
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun observeData() {
+        viewModel.theCar.observe(this) { car ->
+            setInfo(car)
+        }
+    }
+
+    private fun setInfo(car: CarInfo) {
+        binding.carAddDate.text = car.insertionDate
+        binding.carPower.text =
+            String.format(resources.getString(R.string.car_power_template), car.engineCapacity)
+
+        if (car.carName.isEmpty()) {
+            binding.carName.text = resources.getString(R.string.unknown_string)
+        } else {
+            binding.carName.text = car.carName
+        }
+
+        if (car.picturePath.isEmpty()) {
+            binding.ivCarPicture.setImageResource(R.drawable.image_placeholder)
+        } else {
+            binding.ivCarPicture.setImageURI(Uri.parse(car.picturePath))
+        }
+
+        if (car.productionDate.isEmpty()) {
+            binding.carProductionDate.text =
+                String.format(resources.getString(R.string.production_date_template),
+                    resources.getString(R.string.unknown_string))
+        } else {
+            binding.carProductionDate.text =
+                String.format(resources.getString(R.string.production_date_template),
+                    car.productionDate)
+        }
+
+
+    }
 
 }
