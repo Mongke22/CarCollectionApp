fff36744abb86b126b84b934c8c02fba18695a46
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 14:05:49 +0300

    вьюмодель для фрагмента

---
+++ app/src/main/java/com/example/carcollectionapp/presentation/viewmodel/CarsListViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.carcollectionapp.presentation.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.carcollectionapp.data.CarRepositoryImpl
+import com.example.carcollectionapp.domain.CarInfo
+import com.example.carcollectionapp.domain.usecase.GetCarInfoListUseCase
+
+class CarsListViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository = CarRepositoryImpl(application)
+    private val getCarInfoListUseCase = GetCarInfoListUseCase(repository)
+
+    private var _carList = getCarInfoListUseCase()
+    val carList: LiveData<List<CarInfo>>
+        get() = _carList
+
+    fun showCarDetailInfo(id: Int){
+
+    }
+
+}
