014ca40ed0870a74c2a8dc06883c2b8bb500a6c1
Mongke22 <ddd.ddd99.99@mail.ru>
2023-05-31 12:46:38 +0300

    Реаилзация маппера

--- app/src/main/java/com/example/carcollectionapp/data/Mapper.kt
+++ app/src/main/java/com/example/carcollectionapp/data/Mapper.kt
@@ -1,4 +1,24 @@
 package com.example.carcollectionapp.data
 
+import com.example.carcollectionapp.data.database.CarInfoDbModel
+import com.example.carcollectionapp.domain.CarInfo
+
 class Mapper {
+    fun mapDbModelToEntity(carDb: CarInfoDbModel): CarInfo {
+        return CarInfo(
+            id = carDb.id,
+            picturePath = carDb.picturePath,
+            productionDate = carDb.productionDate,
+            engineCapacity = carDb.engineCapacity,
+            insertionDate = carDb.insertionDate)
+    }
+
+    fun mapEntityToDbModel(car: CarInfo): CarInfoDbModel{
+        return CarInfoDbModel(
+            id = 0,
+            picturePath = car.picturePath,
+            productionDate = car.productionDate,
+            engineCapacity = car.engineCapacity,
+            insertionDate = car.insertionDate)
+    }
 }
